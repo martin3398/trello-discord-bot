69a238cc5196e7f2662e85fab98ec8135e20a7f2
Martin Ziegler <martin.ziegler3398@gmail.com>
2022-04-21 15:02:29 +0200

    added pipeline

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,16 @@
 export interface TrelloReceiver {
   onEvent: (event: string, data: string) => void;
 }
+
+export interface Producer<T> {
+  register: (callback: (arg: T) => void) => void;
+  start: () => void;
+}
+
+export interface Consumer<T> {
+  consume: (payload: T) => void;
+}
+
+export interface Middleware<S, T> {
+  transform: (payload: S) => T;
+}
